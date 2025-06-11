e64045e04fd92ec85ef7e6d8e786a4af16d76b7f
Pintu-6545 <pmodi7700@gmail.com>
2025-06-11 12:49:55 +0530

    .

--- Assignment/Module-9) React - Components, State, Props/Lab/first_code/src/Components/WelcomeMessage.jsx
+++ Assignment/Module-9) React - Components, State, Props/Lab/first_code/src/Components/WelcomeMessage.jsx
@@ -1,3 +1,6 @@
+/* Task 2: 
+o Create a class component WelcomeMessage that displays "Welcome to React!" and 
+a render() method.  */
 import React, { Component } from 'react'
 
  class WelcomeMessage extends Component {
